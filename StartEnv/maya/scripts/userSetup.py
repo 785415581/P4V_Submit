fa246c146a2f2c5ea4735e4eb3c52e9591390579
huan <cheng21huan@163.com>
2021-11-10 18:16:11 +0800

    update

--- StartEnv/maya/scripts/userSetup.py
+++ StartEnv/maya/scripts/userSetup.py
@@ -1,5 +1,6 @@
 import sys
 ToolLib = r"R:/ProjectX/Scripts/Python/tools/publish"
+import maya.cmds as cmds
 
 if ToolLib not in sys.path:
     sys.path.append(ToolLib)
@@ -14,8 +15,7 @@ def maya_main_window():
 
 
 def open_window(*args):
-    import sys
-    sys.path.insert(0, "D:/chenghh/gitlab/publish")
+
 
     import AssetBrowser.main as main
     import imp
@@ -39,9 +39,12 @@ def createLevel(*args):
     import Tools.maya.createHierarchy as createHierarchy
     createHierarchy.createHierarchy(asset=True)
 
-import maya.cmds as cmds
-if cmds.menu('AuroraTools', exists=1):
-    cmds.deleteUI('AuroraTools')
-showMyMenu = cmds.menu("AuroraTools", parent="MayaWindow", to=1, aob=1, label=u"JGTools")
-cmds.menuItem(parent=showMyMenu, label='Publish', command=open_window)
-cmds.menuItem(parent=showMyMenu, label='CreateHierarchy', command=createLevel)
+def addJGMenu():
+
+    if cmds.menu('AuroraTools', exists=1):
+        cmds.deleteUI('AuroraTools')
+    showMyMenu = cmds.menu("AuroraTools", parent="MayaWindow", to=1, aob=1, label=u"JGTools")
+    cmds.menuItem(parent=showMyMenu, label='Publish', command=open_window)
+    cmds.menuItem(parent=showMyMenu, label='CreateHierarchy', command=createLevel)
+
+cmds.evalDeferred(addJGMenu)
