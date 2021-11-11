6b530220e08c7e3242220e66808bc08c9329ae7a
Jason <785415581@qq.com>
2021-11-11 15:52:48 +0800

    update: create new branch.

--- StartEnv/bat/runPublish.py
+++ StartEnv/bat/runPublish.py
@@ -1,12 +1,13 @@
 import sys
 import imp
 sys.path.append("R:/ProjectX/Scripts/Python/tools/publish")
-from PySide2 import QtWidgets
+from PySide2 import QtWidgets, QtGui
 import AssetBrowser.main as main
 
         
 imp.reload(main)
 app = QtWidgets.QApplication(sys.argv)
+app.setWindowIcon(QtGui.QIcon("R:/ProjectX/Scripts/Python/tools/publish/AssetBrowser/icon.ico"))
 win = main.MainWindow()
 win.show()
 app.exec_()
