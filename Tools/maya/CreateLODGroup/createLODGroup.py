ae552a5c9309c5be8c925d86a8de35234c8d3e9c
JiaXin <785415581@qq.com>
2024-07-11 19:15:12 +0800

    fix: error

--- Tools/maya/CreateLODGroup/createLODGroup.py
+++ Tools/maya/CreateLODGroup/createLODGroup.py
@@ -1,92 +1,132 @@
-import json
+"""
+   Copyright (C) 2024 Hero company, Inc.
+   All rights reserved.
 
-from PySide2 import QtWidgets
-from PySide2.QtWidgets import QApplication
-from PySide2 import QtCore
-from Tools.maya.CreateLODGroup.UI_LODExportManage import Ui_MainWindow
+   本软件的任何部分或全部不得以任何形式复制、传播、出售、转售或发布，未经本公司的书面许可。
+
+    说明：
+    本软件进行在Maya中预览LOD和导出记录信息功能；在使用之前请理解使用Python、Pyside、Maya API等技术。
+
+    使用方法：
+    1. 测试
+        a. 全局变量有DEBUG，可以设置为True，方便调试
+        b. 单例调试 >python createLODGroup.py
+    2. 界面调整
+        a. 界面全部使用QtDesigner 画制。部分 部件使用自定义，例如 TreeItem 自定义组件
+        b. 导出 .py 文件方法> pyside2-uic.exe LODExportManage.ui>UI_LODExportManage.py
+        c. 如果使用 QtDesigner中添加了svg或者字体图标，可使用 pyside2-rcc.exe 生成 resource_rc.py 文件
+    3. 功能
+        a. 添加LOD组的平台信息
+        b. 预览功能
+        c. 导出功能
+
+    4. 其他
+        a. 未使用 MVC架构，开发流程为瀑布式。
+        b. 直接找信号传递方法，所有功能的出发点
+
+"""
+# -*- coding: utf-8 -*-
+# @Time    : 2024/7/11 18:16
+# @
+# @File    : createLODGroup.py
+# @Software: PyCharm
+
+# system import
 import sys, os
 import json
-from enum import Enum, EnumMeta, unique
+from enum import Enum, unique
 from functools import partial
+# open lib import
+from PySide2 import QtCore
+from PySide2 import QtWidgets
+from PySide2.QtWidgets import QApplication
+# third party import
+from Tools.maya.CreateLODGroup.UI_LODExportManage import Ui_MainWindow
+
+# Test import
 from pprint import pprint
 
 # from maya import cmds
 
+# Test field True or False
 DEBUG = False
 
+# Test variable
 DataBase = {'|LOD_Group_1': {'Check': 'UnCheck',
-                  'LOD_0': {'Level': 'LOD_0',
-                            'UVShell': 1,
-                            'face': 400,
-                            'index': 0,
-                            'longName': '|LOD_Group_1|LOD_0',
-                            'name': '|LOD_Group_1|LOD_0',
-                            'parentGroup': '|LOD_Group_1',
-                            'platform': 'Mobile',
-                            'shapes': ['|LOD_Group_1|LOD_0|pSphere1|pSphereShape1'],
-                            'shortName': 'LOD_0',
-                            'vertex': 382},
-                  'LOD_1': {'Level': 'LOD_1',
-                            'UVShell': 1,
-                            'face': 260,
-                            'index': 1,
-                            'longName': '|LOD_Group_1|LOD_1',
-                            'name': '|LOD_Group_1|LOD_1',
-                            'parentGroup': '|LOD_Group_1',
-                            'platform': 'HD_PC',
-                            'shapes': ['|LOD_Group_1|LOD_1|pSphere1|pSphereShape1',
-                                       '|LOD_Group_1|LOD_1|pSphere1|polySurfaceShape1'],
-                            'shortName': 'LOD_1',
-                            'vertex': 242},
-                  'LOD_2': {'Level': 'LOD_2',
-                            'UVShell': 1,
-                            'face': 180,
-                            'index': 2,
-                            'longName': '|LOD_Group_1|LOD_2',
-                            'name': '|LOD_Group_1|LOD_2',
-                            'parentGroup': '|LOD_Group_1',
-                            'platform': 'Mobile',
-                            'shapes': ['|LOD_Group_1|LOD_2|pSphere1|pSphereShape1',
-                                       '|LOD_Group_1|LOD_2|pSphere1|polySurfaceShape2'],
-                            'shortName': 'LOD_2',
-                            'vertex': 162}},
- '|LOD_Group_2': {'Check': 'UnCheck',
-                  'LOD_0': {'Level': 'LOD_0',
-                            'UVShell': 1,
-                            'face': 400,
-                            'index': 0,
-                            'longName': '|LOD_Group_2|LOD_0',
-                            'name': '|LOD_Group_2|LOD_0',
-                            'parentGroup': '|LOD_Group_2',
-                            'platform': 'Mobile',
-                            'shapes': ['|LOD_Group_2|LOD_0|pSphere2|pSphereShape2'],
-                            'shortName': 'LOD_0',
-                            'vertex': 382},
-                  'LOD_1': {'Level': 'LOD_1',
-                            'UVShell': 1,
-                            'face': 260,
-                            'index': 1,
-                            'longName': '|LOD_Group_2|LOD_1',
-                            'name': '|LOD_Group_2|LOD_1',
-                            'parentGroup': '|LOD_Group_2',
-                            'platform': 'HD_PC',
-                            'shapes': ['|LOD_Group_2|LOD_1|pSphere2|pSphereShape2',
-                                       '|LOD_Group_2|LOD_1|pSphere2|polySurfaceShape3'],
-                            'shortName': 'LOD_1',
-                            'vertex': 242},
-                  'LOD_2': {'Level': 'LOD_2',
-                            'UVShell': 1,
-                            'face': 180,
-                            'index': 2,
-                            'longName': '|LOD_Group_2|LOD_2',
-                            'name': '|LOD_Group_2|LOD_2',
-                            'parentGroup': '|LOD_Group_2',
-                            'platform': 'Mobile',
-                            'shapes': ['|LOD_Group_2|LOD_2|pSphere2|pSphereShape2',
-                                       '|LOD_Group_2|LOD_2|pSphere2|polySurfaceShape4'],
-                            'shortName': 'LOD_2',
-                            'vertex': 162}}}
+                             'LOD_0': {'Level': 'LOD_0',
+                                       'UVShell': 1,
+                                       'face': 400,
+                                       'index': 0,
+                                       'longName': '|LOD_Group_1|LOD_0',
+                                       'name': '|LOD_Group_1|LOD_0',
+                                       'parentGroup': '|LOD_Group_1',
+                                       'platform': 'Mobile',
+                                       'shapes': ['|LOD_Group_1|LOD_0|pSphere1|pSphereShape1'],
+                                       'shortName': 'LOD_0',
+                                       'vertex': 382},
+                             'LOD_1': {'Level': 'LOD_1',
+                                       'UVShell': 1,
+                                       'face': 260,
+                                       'index': 1,
+                                       'longName': '|LOD_Group_1|LOD_1',
+                                       'name': '|LOD_Group_1|LOD_1',
+                                       'parentGroup': '|LOD_Group_1',
+                                       'platform': 'HD_PC',
+                                       'shapes': ['|LOD_Group_1|LOD_1|pSphere1|pSphereShape1',
+                                                  '|LOD_Group_1|LOD_1|pSphere1|polySurfaceShape1'],
+                                       'shortName': 'LOD_1',
+                                       'vertex': 242},
+                             'LOD_2': {'Level': 'LOD_2',
+                                       'UVShell': 1,
+                                       'face': 180,
+                                       'index': 2,
+                                       'longName': '|LOD_Group_1|LOD_2',
+                                       'name': '|LOD_Group_1|LOD_2',
+                                       'parentGroup': '|LOD_Group_1',
+                                       'platform': 'Mobile',
+                                       'shapes': ['|LOD_Group_1|LOD_2|pSphere1|pSphereShape1',
+                                                  '|LOD_Group_1|LOD_2|pSphere1|polySurfaceShape2'],
+                                       'shortName': 'LOD_2',
+                                       'vertex': 162}},
+            '|LOD_Group_2': {'Check': 'UnCheck',
+                             'LOD_0': {'Level': 'LOD_0',
+                                       'UVShell': 1,
+                                       'face': 400,
+                                       'index': 0,
+                                       'longName': '|LOD_Group_2|LOD_0',
+                                       'name': '|LOD_Group_2|LOD_0',
+                                       'parentGroup': '|LOD_Group_2',
+                                       'platform': 'Mobile',
+                                       'shapes': ['|LOD_Group_2|LOD_0|pSphere2|pSphereShape2'],
+                                       'shortName': 'LOD_0',
+                                       'vertex': 382},
+                             'LOD_1': {'Level': 'LOD_1',
+                                       'UVShell': 1,
+                                       'face': 260,
+                                       'index': 1,
+                                       'longName': '|LOD_Group_2|LOD_1',
+                                       'name': '|LOD_Group_2|LOD_1',
+                                       'parentGroup': '|LOD_Group_2',
+                                       'platform': 'HD_PC',
+                                       'shapes': ['|LOD_Group_2|LOD_1|pSphere2|pSphereShape2',
+                                                  '|LOD_Group_2|LOD_1|pSphere2|polySurfaceShape3'],
+                                       'shortName': 'LOD_1',
+                                       'vertex': 242},
+                             'LOD_2': {'Level': 'LOD_2',
+                                       'UVShell': 1,
+                                       'face': 180,
+                                       'index': 2,
+                                       'longName': '|LOD_Group_2|LOD_2',
+                                       'name': '|LOD_Group_2|LOD_2',
+                                       'parentGroup': '|LOD_Group_2',
+                                       'platform': 'Mobile',
+                                       'shapes': ['|LOD_Group_2|LOD_2|pSphere2|pSphereShape2',
+                                                  '|LOD_Group_2|LOD_2|pSphere2|polySurfaceShape4'],
+                                       'shortName': 'LOD_2',
+                                       'vertex': 162}}}
 
+
+# Node Type
 @unique
 class NodeType(Enum):
     NodeDir = 0;
@@ -108,8 +148,10 @@ class ExportLOD(QtWidgets.QMainWindow):
             self.AllLODItems = DataBase
         else:
             self.setWindowTitle("LOD Export")
+            from maya import cmds as cmds
             # 获取场景中的所有LOD 信息，给后面的界面填充数据
             self.AllLODItems = self.CollectLodInfo()
+        #设置当前 stackedWidget index
         self.MainUI.stackedWidget.setCurrentIndex(0)
         if not self.AllLODItems:
             self.MainUI.label_4.setText("None LOD Group")
@@ -134,11 +176,13 @@ class ExportLOD(QtWidgets.QMainWindow):
 
     def TreeItemChanged(self, item, column):
         """ 树节点改变时 """
-        if item.data(1, QtCore.Qt.UserRole) == "Parent":
-            if item.checkState(column) == QtCore.Qt.Checked:
-                self.AllLODItems[item.text(column)]["Check"] = "Check"
-            else:
-                self.AllLODItems[item.text(column)]["Check"] = "Uncheck"
+        if item.text(column):
+            # 判断是否是父节点
+            if item.data(1, QtCore.Qt.UserRole) == "Parent":
+                if item.checkState(column) == QtCore.Qt.Checked:
+                    self.AllLODItems[item.text(column)]["Check"] = "Check"
+                else:
+                    self.AllLODItems[item.text(column)]["Check"] = "Uncheck"
 
     # def RegisterEventCallBack(self):
     #     """ 注册回调"""
@@ -148,7 +192,6 @@ class ExportLOD(QtWidgets.QMainWindow):
 
     def Export(self):
         """ 导出LOD 数据 """
-
         pprint(self.AllLODItems)
         from maya import cmds
         checked_items = []
@@ -325,6 +368,7 @@ class ExportLOD(QtWidgets.QMainWindow):
     def RebuildTreeView(self):
         """ 清除之前的Tree， 重新创建Tree， 因为有数据更新，创建出来的Tree要记录上次的设置信息"""
         self.MainUI.treeWidget.clear()
+        # TODO: 未知原因，空场景开工具无法进行刷新，只能再次打开工具。
         # 暂时停止信号， 在写入Data 的时候userRole数据会发生信号传递
         self.MainUI.treeWidget.itemChanged.disconnect(self.TreeItemChanged)
         self.RootNode = QtWidgets.QTreeWidgetItem(self.MainUI.treeWidget, type=NodeType.NodeDir.value)
@@ -335,6 +379,10 @@ class ExportLOD(QtWidgets.QMainWindow):
             self.AllLODItems = self.AllLODItems
         else:
             self.AllLODItems = self.ReCollectLodInfo()
+        print("-----------------")
+        pprint(self.AllLODItems)
+        print("-----------------")
+
         for name, LodInfo in self.AllLODItems.items():
             ChildNode = QtWidgets.QTreeWidgetItem(self.RootNode, type=NodeType.NodeDir.value)
             ChildNode.setText(0, name)
@@ -350,8 +398,18 @@ class ExportLOD(QtWidgets.QMainWindow):
 
     def ReCollectLodInfo(self):
         """ 重新组织Lod 信息， 为后面的输出做准备"""
+        from maya import cmds as cmds
         lodGroups = cmds.ls(type="lodGroup", long=True)
-        lodDetail = self.AllLODItems
+        if len(lodGroups) == 0:
+            cmds.warning("No lodGroup found in the scene")
+            self.AllLODItems = {}
+        #  TODO: 只要当前场景中存在的Lod 组
+        newAllLODItems = {}
+        for lodGroup in lodGroups:
+            if lodGroup in self.AllLODItems.keys():
+                newAllLODItems[lodGroup] = self.AllLODItems[lodGroup]
+
+        lodDetail = newAllLODItems
         for lodGroup in lodGroups:
             # 添加新的Lod节点信息
             if lodGroup not in self.AllLODItems:
